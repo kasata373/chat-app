1e4938d10e8f9f9b7f5c4ccdbdc6decf2c9ae9a2
kasata373 <falken783@gmail.com>
2021-01-17 02:55:32 +0900

    投稿時刻の日本時間への修正

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module ChatApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.li8n.default_locale = :ja
+    config.time_zone = 'Tokyo'
 
     config.generators do |g|
       g.styleshhets false
